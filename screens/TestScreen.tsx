4a679c84c62ea6dac3f8fd6c8cfb755360ae6185
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-11 01:24:16 +0800

    Add StatusBar and toggle functionality in TestScreen

--- screens/TestScreen.tsx
+++ screens/TestScreen.tsx
@@ -1,12 +1,22 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text, Button } from "react-native";
 
 const TestScreen = ({ route, navigation }: { navigation: any; route: any }) => {
-  const { name } = route.params;
+  const name = route?.params?.name ?? 'DefaultName';
+  const [isVisible, setIsVisible] = React.useState(false);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hi {name}</Text>
       <Button title="Go To Home" onPress={() => navigation.navigate("Home")} />
+      <Button
+        title="Toggle StatusBar"
+        onPress={() => {
+          setIsVisible(!isVisible);
+        }}
+      />
+      <StatusBar hidden={isVisible} />
     </View>
   );
 };
